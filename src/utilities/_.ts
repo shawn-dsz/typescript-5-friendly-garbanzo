b05f1820aaf2b4512d00f57bf45d3de8b1c6e01f
Shawn D'souza <shawn-dsz@users.noreply.github.com>
2023-03-15 13:59:22 +1100

    renmane

--- src/utilities/_.ts
+++ src/utilities/_.ts
@@ -2,3 +2,6 @@
 
 // * the utility types are a set of types that are available globally in all typescript projects
 // *  so you don't need to add any dependencies to get this going every utility type looks something this..
+
+
+// ~ https://www.typescriptlang.org/docs/handbook/utility-types.html
